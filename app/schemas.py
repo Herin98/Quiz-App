3eb9d72d9d9a4d94f688a481130467aa29a1afcc
Herin98 <142152236+Herin98@users.noreply.github.com>
2023-12-24 17:39:58 +0530

    Delete app directory

--- app/schemas.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields, base
-
-
-"""
-This module aims at providing the request and response format for the various api calls.
-This also helpful for creating swagger docs for apis testing.
-"""
